00cba4bd49762ea16a150961a8a2e62ff8606b10
xiwang.guo <xiwang.guo@zkj.com>
2020-11-25 17:51:21 +0800

    Initial commit

---
+++ app/src/main/java/com/baidu/ocr/demo/ConsUtil.java
@@ -0,0 +1,7 @@
+package com.baidu.ocr.demo;
+
+public class ConsUtil {
+
+    public static final String appKey = "dV6YuUXGCGzd9dqjPwff0RKg";
+    public static final String secretKey = "qxAlFps3BCfsGFDhrWabk85qadSV6buG";
+}
